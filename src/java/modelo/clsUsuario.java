7827bd0afd062de8da80705e346aad1abf14ffaf
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-03-25 08:45:01 -0600

    Add the initial structure for the class clsUsuario

--- src/java/modelo/clsUsuario.java
+++ src/java/modelo/clsUsuario.java
@@ -1,14 +1,81 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Estructura de la entidad USUARIO que se utilizará
+ *   -nombre        (String)
+ *   -usuario       (String)
+ *   -contraseña    (String)
+ *   -ruta          (String)
+ *   -tipo_usuario  (String)
  */
 package modelo;
 
+import java.sql.Connection;
+
 /**
  *
- * @author aleja
+ * @author Jonathan Alejandro Sánchez Alcántara
  */
 public class clsUsuario {
     
+    private String nombre;
+    private String usuario;
+    private String contrasena;
+    private String ruta;
+    private String tipoUsuario;
+    
+    public clsUsuario(){ //Constructor base
+        
+    }
+    
+    public clsUsuario(String usuario, String contrasena){
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+    
+    // ------------------------
+    // Sección de manejo de conexión y manipulació de datos
+    // ------------------------
+    Connection cn;
+
+    // ------------------------
+    // Setters and Getters
+    // ------------------------
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }   
 }
