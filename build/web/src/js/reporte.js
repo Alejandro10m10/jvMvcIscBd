d6de76a169033cd2c3e909dea6a01946a0ce59e7
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-04-07 01:53:48 -0500

    Add scroll viewer to move page scroll

--- build/web/src/js/reporte.js
+++ build/web/src/js/reporte.js
@@ -4,6 +4,7 @@ const userPictureContent = document.querySelector('.picture');
 const userPictureDiv =document.querySelector('.user-pictrue');
 const btnCloseImg = document.querySelector('#btnCloseImage');
 const btnCloseBackground = document.querySelector('.user-pictrue__background');
+const body = document.querySelector('body');
 
 // Eventos
 
@@ -30,11 +31,14 @@ function maximizeImage(e){
     img.setAttribute('alt', "Foto de perfil de usuario");
     img.classList.add('user-pictrue__picture');
     userPictureContent.appendChild(img);    
+    body.classList.add('no-show-scroll');
+    moveScroll();
 }
 
 
 function closePictureDiv(){
     userPictureDiv.classList.add('no-display');
+    body.classList.remove('no-show-scroll');
     limpiarPictureDiv();
 }
 
@@ -44,4 +48,13 @@ function limpiarPictureDiv(){
         userPictureContent.removeChild(userPictureContent.firstChild);
     }
     
+}
+
+/* Variables that allow us to save the position of the scroll and change it */
+let scrrollOntop;
+let doc = document.documentElement;
+function moveScroll(){
+    scrrollOntop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    doc.scrollTop = scrrollOntop;
+    userPictureDiv.style.top = `${scrrollOntop}px`;
 }
